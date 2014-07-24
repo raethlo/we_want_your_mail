1ec86b52aa2043ebf470439b0bea96318773608c
raethlo <roman.rostar@gmail.com>
2014-07-24 11:03:22 +0200

    saving user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
 
   def index
-
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      #redirect_to thanks
+    else
+    end
+  end
 
+  def user_params
+    params.require(:user).permit(:email)
   end
 end
